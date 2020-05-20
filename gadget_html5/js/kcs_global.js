903cca180ccc4d40744f09c9a13f235c2b816601
gk <gkpyon@gmail.com>
2020-05-20 18:00:00 +0900

    2020.05.20

---
+++ gadget_html5/js/kcs_global.js
@@ -0,0 +1,28 @@
+﻿// Viewer 情報
+var viewerInfo = {};
+viewerInfo.id               = "";
+viewerInfo.displayName      = "";
+viewerInfo.userType         = "";
+
+// Flash 情報
+var flashInfo = {};
+flashInfo.apiToken          = "";
+flashInfo.apiStartTime      = "";
+flashInfo.dmmLoginId        = "";
+
+// Item 情報
+var itemInfo = {};
+itemInfo.skuId              = "";
+itemInfo.price              = "";
+itemInfo.count              = "";
+itemInfo.description        = "";
+itemInfo.name               = "";
+itemInfo.imageUrl           = "";
+
+// Content 情報
+var contentInfo = {};
+contentInfo.id              = "";
+
+// UserWorld 情報
+var userWorldInfo = {};
+userWorldInfo.worldServerAddr = "";
