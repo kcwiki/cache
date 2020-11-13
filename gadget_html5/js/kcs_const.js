149615140eae4e240f626b1cd14647e91845223b
gk <gkpyon@gmail.com>
2020-11-13 18:00:00 +0900

    2020.11.13

--- gadget_html5/js/kcs_const.js
+++ gadget_html5/js/kcs_const.js
@@ -54,12 +54,12 @@ ConnectionInfo.Interval_Min   = 10;
 var MaintenanceInfo = {};
 MaintenanceInfo.IsDoing       = 0;
 MaintenanceInfo.IsEmergency   = 0;
-MaintenanceInfo.StartDateTime = Date.parse("2020/10/16 11:00:00");
-MaintenanceInfo.EndDateTime   = Date.parse("2020/10/16 19:30:00");
+MaintenanceInfo.StartDateTime = Date.parse("2020/11/13 11:00:00");
+MaintenanceInfo.EndDateTime   = Date.parse("2020/11/13 19:30:00");
 
 // Version 情報
 var VersionInfo = {};
-VersionInfo.scriptVesion      = "5.0.2.1";
+VersionInfo.scriptVesion      = "5.0.3.1";
 
 // Entrance 情報
 var EntranceInfo = {};
