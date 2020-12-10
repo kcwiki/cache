51783cfdb3d4f6734a31cfe6ad8f8d10550cf766
gk <gkpyon@gmail.com>
2020-12-10 18:00:00 +0900

    2020.12.10

--- gadget_html5/js/kcs_const.js
+++ gadget_html5/js/kcs_const.js
@@ -59,7 +59,7 @@ MaintenanceInfo.EndDateTime   = Date.parse("2020/11/28 02:00:00");
 
 // Version 情報
 var VersionInfo = {};
-VersionInfo.scriptVesion      = "5.1.0.0";
+VersionInfo.scriptVesion      = "5.1.0.3";
 
 // Entrance 情報
 var EntranceInfo = {};
