c16b3ca3c3d5a1c4c14077a791d3cd915e7950c4
Botkaze <gkpyon+1@gmail.com>
2022-06-14 12:55:03 +0900

    2022-06-14

--- gadget_html5/js/kcs_const.js
+++ gadget_html5/js/kcs_const.js
@@ -59,7 +59,7 @@ MaintenanceInfo.EndDateTime   = Date.parse("2022/06/08 00:59:59");
 
 // Version 情報
 var VersionInfo = {};
-VersionInfo.scriptVesion      = "5.4.0.6";
+VersionInfo.scriptVesion      = "5.4.0.7";
 
 // Entrance 情報
 var EntranceInfo = {};
