51f31db3bc2fc85612a0ebd340612cd4b9ee46dd
Botkaze <gkpyon+1@gmail.com>
2024-01-01 00:15:02 +0900

    2023-12-31

--- gadget_html5/js/kcs_const.js
+++ gadget_html5/js/kcs_const.js
@@ -59,7 +59,7 @@ MaintenanceInfo.EndDateTime   = Date.parse("2023/12/26 00:59:59");
 
 // Version 情報
 var VersionInfo = {};
-VersionInfo.scriptVesion      = "5.8.1.0";
+VersionInfo.scriptVesion      = "5.8.1.1";
 
 // Entrance 情報
 var EntranceInfo = {};
