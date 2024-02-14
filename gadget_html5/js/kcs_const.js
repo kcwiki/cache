ecdc38ae39a84d6dabb1e81e6276e30eb7fcf2a0
Botkaze <gkpyon+1@gmail.com>
2024-02-14 18:37:28 +0900

    2024-02-14

--- gadget_html5/js/kcs_const.js
+++ gadget_html5/js/kcs_const.js
@@ -54,12 +54,12 @@ ConnectionInfo.Interval_Min   = 10;
 var MaintenanceInfo = {};
 MaintenanceInfo.IsDoing       = 0;
 MaintenanceInfo.IsEmergency   = 0;
-MaintenanceInfo.StartDateTime = Date.parse("2024/01/25 00:00:00");
-MaintenanceInfo.EndDateTime   = Date.parse("2024/01/25 00:59:59");
+MaintenanceInfo.StartDateTime = Date.parse("2024/02/13 00:00:00");
+MaintenanceInfo.EndDateTime   = Date.parse("2024/02/13 00:59:59");
 
 // Version 情報
 var VersionInfo = {};
-VersionInfo.scriptVesion      = "5.8.2.1";
+VersionInfo.scriptVesion      = "5.8.3.0";
 
 // Entrance 情報
 var EntranceInfo = {};
