1b7f59c9792030b88595cb6e639f99a774188d68
Botkaze <gkpyon+1@gmail.com>
2025-10-16 13:43:08 +0900

    2025-10-16

--- gadget_html5/js/kcs_const.js
+++ gadget_html5/js/kcs_const.js
@@ -1,91 +1,44 @@
-﻿// ガジェット情報
+﻿var KCS_PROTOCOL = "https://";
+var KCS_DOMAIN = ".kancolle-server.com";
+
 var ConstGadgetInfo = {};
-ConstGadgetInfo.height = 860; //1160;
+ConstGadgetInfo.height = 860;
 
-// SNS 情報
-var ConstSNSInfo = {};
-ConstSNSInfo.id = "DMM";
-
-// サーバー情報
 var ConstServerInfo = {};
-ConstServerInfo.Gadget           = "http://w00g.kancolle-server.com/";
-ConstServerInfo.World_1          = "http://w01y.kancolle-server.com/";
-ConstServerInfo.World_2          = "http://w02k.kancolle-server.com/";
-ConstServerInfo.World_3          = "http://w03s.kancolle-server.com/";
-ConstServerInfo.World_4          = "http://w04m.kancolle-server.com/";
-ConstServerInfo.World_5          = "http://w05o.kancolle-server.com/";
-ConstServerInfo.World_6          = "http://w06t.kancolle-server.com/";
-ConstServerInfo.World_7          = "http://w07l.kancolle-server.com/";
-ConstServerInfo.World_8          = "http://w08r.kancolle-server.com/";
-ConstServerInfo.World_9          = "http://w09s.kancolle-server.com/";
-ConstServerInfo.World_10         = "http://w10b.kancolle-server.com/";
-ConstServerInfo.World_11         = "http://w11t.kancolle-server.com/";
-ConstServerInfo.World_12         = "http://w12p.kancolle-server.com/";
-ConstServerInfo.World_13         = "http://w13b.kancolle-server.com/";
-ConstServerInfo.World_14         = "http://w14h.kancolle-server.com/";
-ConstServerInfo.World_15         = "http://w15p.kancolle-server.com/";
-ConstServerInfo.World_16         = "http://w16s.kancolle-server.com/";
-ConstServerInfo.World_17         = "http://w17k.kancolle-server.com/";
-ConstServerInfo.World_18         = "http://w18i.kancolle-server.com/";
-ConstServerInfo.World_19         = "http://w19s.kancolle-server.com/";
-ConstServerInfo.World_20         = "http://w20h.kancolle-server.com/";
-ConstServerInfo.OSAPI            = "osapi.dmm.com";
-ConstServerInfo.NETGAME          = "http://www.dmm.com";
+ConstServerInfo.Gadget = KCS_PROTOCOL + "w00g" + KCS_DOMAIN + "/";
+ConstServerInfo.World = 
+[
+    "",
+    KCS_PROTOCOL + "w01y" + KCS_DOMAIN + "/",
+    KCS_PROTOCOL + "w02k" + KCS_DOMAIN + "/",
+    KCS_PROTOCOL + "w03s" + KCS_DOMAIN + "/",
+    KCS_PROTOCOL + "w04m" + KCS_DOMAIN + "/",
+    KCS_PROTOCOL + "w05o" + KCS_DOMAIN + "/",
+    KCS_PROTOCOL + "w06t" + KCS_DOMAIN + "/",
+    KCS_PROTOCOL + "w07l" + KCS_DOMAIN + "/",
+    KCS_PROTOCOL + "w08r" + KCS_DOMAIN + "/",
+    KCS_PROTOCOL + "w09s" + KCS_DOMAIN + "/",
+    KCS_PROTOCOL + "w10b" + KCS_DOMAIN + "/",
+    KCS_PROTOCOL + "w11t" + KCS_DOMAIN + "/",
+    KCS_PROTOCOL + "w12p" + KCS_DOMAIN + "/",
+    KCS_PROTOCOL + "w13b" + KCS_DOMAIN + "/",
+    KCS_PROTOCOL + "w14h" + KCS_DOMAIN + "/",
+    KCS_PROTOCOL + "w15p" + KCS_DOMAIN + "/",
+    KCS_PROTOCOL + "w16s" + KCS_DOMAIN + "/",
+    KCS_PROTOCOL + "w17k" + KCS_DOMAIN + "/",
+    KCS_PROTOCOL + "w18i" + KCS_DOMAIN + "/",
+    KCS_PROTOCOL + "w19s" + KCS_DOMAIN + "/",
+    KCS_PROTOCOL + "w20h" + KCS_DOMAIN + "/"
+];
+ConstServerInfo.OSAPI = "osapi.dmm.com";
+ConstServerInfo.NETGAME = KCS_PROTOCOL + "www.dmm.com";
 
-// URL 情報
 var ConstURLInfo = {};
-ConstURLInfo.GetUserWorldURL     = ConstServerInfo.Gadget + "kcsapi/api_world/get_id/";
-ConstURLInfo.ConnectionCheckURL  = ConstServerInfo.Gadget + "index.html";
-ConstURLInfo.LoginURL            = "kcsapi/api_auth_member/dmmlogin/";
+ConstURLInfo.ConnectionCheckURL = ConstServerInfo.Gadget + "index.html";
+ConstURLInfo.LoginURL = "kcsapi/api_auth_member/dmmlogin/";
 
-// Parameter 情報
-var ConstParameterInfo = {};
-ConstParameterInfo.PaymentResult_Paid   =  0;
-ConstParameterInfo.PaymentResult_Cancel = -1;
-ConstParameterInfo.GetUserWorld_IsNew   = 0;
-ConstParameterInfo.APIResult_OK         = 1;
-ConstParameterInfo.APIResult_NG_Reload  = -2;
-
-// Connection 情報
-var ConnectionInfo = {};
-ConnectionInfo.Interval_Min   = 10;
-
-// Maintenance 情報
 var MaintenanceInfo = {};
-MaintenanceInfo.IsDoing       = 1;
-MaintenanceInfo.IsEmergency   = 0;
-MaintenanceInfo.StartDateTime = Date.parse("2025/10/16 00:00:00");
-MaintenanceInfo.EndDateTime   = Date.parse("2025/10/16 00:59:59");
+MaintenanceInfo.IsDoing = 1;
 
-// Version 情報
 var VersionInfo = {};
-VersionInfo.scriptVesion      = "6.1.6.0";
-
-// Entrance 情報
-var EntranceInfo = {};
-EntranceInfo.Groups           = 10;
-EntranceInfo.Interval_Min     = 1;
-EntranceInfo.UidIndex         = 0;
-
-// Entrance 情報（ワールド別）
-EntranceInfo.NewUser          = 0;
-EntranceInfo.World_1_User     = 0;
-EntranceInfo.World_2_User     = 0;
-EntranceInfo.World_3_User     = 0;
-EntranceInfo.World_4_User     = 0;
-EntranceInfo.World_5_User     = 0;
-EntranceInfo.World_6_User     = 0;
-EntranceInfo.World_7_User     = 0;
-EntranceInfo.World_8_User     = 0;
-EntranceInfo.World_9_User     = 0;
-EntranceInfo.World_10_User    = 0;
-EntranceInfo.World_11_User    = 0;
-EntranceInfo.World_12_User    = 0;
-EntranceInfo.World_13_User    = 0;
-EntranceInfo.World_14_User    = 0;
-EntranceInfo.World_15_User    = 0;
-EntranceInfo.World_16_User    = 0;
-EntranceInfo.World_17_User    = 0;
-EntranceInfo.World_18_User    = 0;
-EntranceInfo.World_19_User    = 0;
-EntranceInfo.World_20_User    = 0;
+VersionInfo.scriptVesion = "6.1.7.0";
--- gadget_html5/js/kcs_login.js
+++ gadget_html5/js/kcs_login.js
@@ -1,7 +1,80 @@
 ﻿function kcsLogin_StartLogin()
 {
-    kcsRenderScreen(ConstServerInfo.Gadget + "html/maintenance.html");
-    return;
+    var params = {};
+    params[opensocial.DataRequest.PeopleRequestFields.PROFILE_DETAILS] = [
+        "userType"
+    ];
+    var request = opensocial.newDataRequest();
+    request.add(
+        request.newFetchPersonRequest(
+            opensocial.IdSpec.PersonId.VIEWER, params), "viewer"
+    );
+    request.send(function(response)
+    {
+        if (!response.hadError()) {
+            var viewer = response.get("viewer").getData();
+            const staff = ["developer", "staff"];
+            if (MaintenanceInfo.IsDoing == 1 && staff.indexOf(viewer.getField("userType")) == -1) {
+                kcsRenderScreen(ConstServerInfo.Gadget + "html/maintenance.html");
+                return;
+            }
+            var worldParams = {};
+            worldParams[gadgets.io.RequestParameters.METHOD] = gadgets.io.MethodType.GET;
+            worldParams[gadgets.io.RequestParameters.CONTENT_TYPE] = gadgets.io.ContentType.JSON;
+            var date = new Date();
+            gadgets.io.makeRequest(ConstServerInfo.Gadget + "kcsapi/api_world/get_id/" + viewer.getId() + "/1/" + date.getTime(), function(worldResponse) {
+                var json = eval(worldResponse.text);
+                if(json.api_result == 1) {
+                    var userWorldId = json.api_data.api_world_id;
+
+                    if(userWorldId < 1)
+                    {
+                        kcsRenderScreen(ConstServerInfo.Gadget + "kcs2/world.html?osapi_root=" + ConstServerInfo.OSAPI);
+                        return;
+                    }
+
+                    var worldServerAddr = ConstServerInfo.World[userWorldId];
+                    var loginParams = {};
+                    loginParams[gadgets.io.RequestParameters.METHOD] = gadgets.io.MethodType.GET;
+                    loginParams[gadgets.io.RequestParameters.CONTENT_TYPE] = gadgets.io.ContentType.JSON;
+                    loginParams[gadgets.io.RequestParameters.AUTHORIZATION] = gadgets.io.AuthorizationType.SIGNED;
+                    var date = new Date();
+                    gadgets.io.makeRequest(worldServerAddr + ConstURLInfo.LoginURL + viewer.getId() + "/1/" + date.getTime(), function(response) {
+                        var json = eval(response.text);
+                        if(json.api_result == 1) {
+                            userWorldInfo.worldServerAddr = worldServerAddr.substr(0, worldServerAddr.length - 1);
+                            var url = worldServerAddr
+                                    + "kcs2/index.php?"
+                                    + "api_root=/kcsapi&"
+                                    + "voice_root=/kcs/sound&"
+                                    + "osapi_root=" + ConstServerInfo.OSAPI + "&"
+                                    + "version=" + VersionInfo.scriptVesion + "&"
+                                    + "api_token=" + json.api_token + "&"
+                                    + "api_starttime=" + json.api_starttime;
+                            kcsRenderScreen(url);
+                            return;
+                        }
+                        else
+                        {
+                            var url = ConstServerInfo.Gadget + "html/";
+                            if(json.api_result == 301){
+                                url = url + "ban.html";
+                            } else if(json.api_result == 302) {
+                                url = url + "deny.html";
+                            } else {
+                                url = url + "error.html";
+                            }
+                            kcsRenderScreen(url);
+                            return;
+                        }
+                    }, loginParams);
+                } else {
+                    kcsRenderScreen(ConstServerInfo.Gadget + "html/maintenance.html");
+                    return;
+                }
+            }, worldParams);
+        }
+    });
 }
 
 function kcsRenderScreen(url)
